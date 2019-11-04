59ee94c15c4b919406e3e4bc6ee2a0c09e187136
Almasud <almasud.arm@gmail.com>
2019-11-04 23:36:48 +0600

    Setup google calendar

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 import os
 import time
 import speech_recognition as sr
 import playsound
 from gtts import gTTS
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def speak(text):
     tts = gTTS(text=text, lang="en")
@@ -18,15 +27,48 @@ def get_audio():
         try:
             said = r.recognize_google(audio)
             print("You say: " + said)
-        except Exception as e:
-            print("Exception: " + str(e))
+        except LookupError as err:
+            print("Opps! could not understand audio: " + str(err))
     return said
 
-text = get_audio()
+# Code for google calender
+def authenticate_google_calender():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-if "hello" in text:
-    speak("Hello, How are you")
-if "what's your name" or "what is your name" in text:
-    speak("My name is, Shikha")
+    service = build('calendar', 'v3', credentials=creds)
+    return service
 
+def get_calendar_event(n, service):
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print(f'Getting the upcoming {n} events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=n, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+service = authenticate_google_calender()
+get_calendar_event(2, service)
 
